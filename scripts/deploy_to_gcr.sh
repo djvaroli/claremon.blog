ff729019a8c2e888453c9be506b1c1662e3d4cd1
Daniel Varoli <daniel.varoli@gmail.com>
2021-03-15 23:16:21 -0400

    minor changes for quick push

--- scripts/deploy_to_gcr.sh
+++ scripts/deploy_to_gcr.sh
@@ -1,17 +1,12 @@
 deploy_to_gcr () {
     GCP_PROJECT=$1
     GCP_SERVICE=$2
+    GCP_REGION=$3
     tag=$(git rev-parse HEAD)
     image="gcr.io/$GCP_PROJECT/$GCP_SERVICE:$tag"
-    echo $image
 
-
-    # gcloud builds submit --conifg _build/cloudbild.yaml --substitutions _IMAGE=$image
-
-    #     exec ["gcloud", "beta", "run", "deploy", SERVICE,
-    #     "--platform", "managed", "--region", "us-central1", 
-    #     "--allow-unauthenticated", "--image", image, "--concurrency", "80"]
-
+    gcloud builds submit --conifg _build/cloudbild.yaml --substitutions _IMAGE=$image
+    gcloud beta run deploy $GCP_SERVICE --platform managed --region $GCP_REGION --allow-unauthenticated --image $image --concurrency 80
 }
 
 "$@"
